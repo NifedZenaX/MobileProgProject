b10dce0ad8f57ce957dd3ab886dbb58b7ec8cdeb
NifedZenaX <soo.willie@yahoo.co.id>
2023-01-17 23:11:08 +0700

    view all transactions

---
+++ app/src/main/java/id/ac/binus/mobileprog/mobileprogproject/TransactionAdapter.java
@@ -0,0 +1,66 @@
+package id.ac.binus.mobileprog.mobileprogproject;
+
+import android.content.Context;
+import android.icu.number.NumberFormatter;
+import android.view.ContentInfo;
+import android.view.ContextMenu;
+import android.view.LayoutInflater;
+import android.view.SurfaceControl;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Date;
+import java.util.List;
+
+public class TransactionAdapter extends BaseAdapter {
+
+    List<String> description, categoryName;
+    List<Integer> nominal;
+    List<Date> date;
+    LayoutInflater inflater;
+
+    public TransactionAdapter(Context context, List<String> description, List<String> categoryName, List<Integer> nominal, List<Date> date) {
+        this.description = description;
+        this.categoryName = categoryName;
+        this.nominal = nominal;
+        this.date = date;
+        inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public int getCount() {
+        return description.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        view = inflater.inflate(R.layout.activity_transaction, null);
+        TextView desc = view.findViewById(R.id.descriptionTextView);
+        TextView cat = view.findViewById(R.id.categoryTextView);
+        TextView dateTxt = view.findViewById(R.id.dateTextView);
+        TextView nominal = view.findViewById(R.id.nominalTextView);
+
+        desc.setText(description.get(i));
+        cat.setText(categoryName.get(i));
+        dateTxt.setText(date.get(i).toString());
+
+        DecimalFormat formatter = new DecimalFormat("#.###");
+        String nominalTxt = formatter.format(this.nominal.get(i).toString());
+        nominal.setText("Rp. " + nominalTxt);
+        return view;
+    }
+}
--- app/src/main/java/id/ac/binus/mobileprog/mobileprogproject/ViewTransactionsActivity.java
+++ app/src/main/java/id/ac/binus/mobileprog/mobileprogproject/ViewTransactionsActivity.java
@@ -2,11 +2,16 @@ package id.ac.binus.mobileprog.mobileprogproject;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -14,34 +19,60 @@ import com.google.firebase.firestore.QuerySnapshot;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 public class ViewTransactionsActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
     FirebaseFirestore db;
+
+    List<String> descriptions;
+    List<String> categoryNames;
+    List<Date> dates;
+    List<Integer> nominals;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_transactions);
 
+        recyclerView = findViewById(R.id.recyclerView);
+
+
         db = FirebaseFirestore.getInstance();
 
-        db.collection("transactions").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                if(task.isSuccessful()){
-                    for (QueryDocumentSnapshot doc: task.getResult()) {
-                        String category = doc.get("category_id").toString();
-                        String description = doc.get("description").toString();
-                        int nominal = Integer.parseInt(doc.get("nominal").toString());
-                        try {
-                            Date date = new SimpleDateFormat("dd/MM/yyyy").parse(doc.get("date").toString());
-                        } catch (ParseException e) {
-                            throw new RuntimeException(e);
+        db.collection("transactions")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                    if(task.isSuccessful()){
+                        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                        for (QueryDocumentSnapshot doc: task.getResult()) {
+                            if(doc.get("user_id").toString() == user.getUid()){
+                                descriptions.add(doc.get("description").toString());
+                                nominals.add(Integer.parseInt(doc.get("nominal").toString()));
+                                try {
+                                    dates.add(new SimpleDateFormat("dd/MM/yyyy").parse(doc.get("date").toString()));
+                                } catch (ParseException e) {
+                                    throw new RuntimeException(e);
+                                }
+                                db.collection("categories")
+                                        .document(doc.get("category_id").toString())
+                                        .get()
+                                        .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                                if(task.isSuccessful()){
+                                                    categoryNames.add(task.getResult().get("name").toString());
+                                                }
+                                            }
+                                        });
+                            }
                         }
                     }
                 }
-            }
         });
+        TransactionAdapter adapter = new TransactionAdapter(getApplicationContext(), descriptions, categoryNames, nominals, dates);
     }
 }
